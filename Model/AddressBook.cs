3ec274ef52c8408855a8e49e983db28004c8c2db
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-09-15 13:51:44 +0200

    test: add addressbook - add contact

---
+++ Model/AddressBook.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+
+namespace CRM
+{
+    public class AddressBook
+    {
+
+    }
+}
---
+++ TestModel/TestAddressBook.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System;
+
+namespace CRM
+{
+    public class TestAddressBook
+    {
+        #region private attributes
+        private AddressBook _addressBook;
+        #endregion private attributes
+
+        [SetUp]
+        public void Setup()
+        {
+            _addressBook = new AddressBook();
+        }
+
+        [Test]
+        public void Contacts_AddOneContact_GetContacts()
+        {
+            //given
+            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16),"espagnole", "jp@gauthier.com");
+
+            //when
+            _addressBook.AddContact(gauthier);
+
+            //then
+            Assert.AreEqual(1, _addressBook.Contacts.Count());
+
+        }
+    }
+}
