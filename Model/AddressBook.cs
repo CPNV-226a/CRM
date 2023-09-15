aca787a7c0dffc62c63a355018d2d31e28aa0e8d
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-09-15 15:59:20 +0200

    chore: update architecture with addContact

--- Model/AddressBook.cs
+++ Model/AddressBook.cs
@@ -5,5 +5,17 @@ namespace CRM
 {
     public class AddressBook
     {
+        public void AddContact(Contact contactToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> Contacts
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
