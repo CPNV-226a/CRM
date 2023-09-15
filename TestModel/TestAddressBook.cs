18ccad97d825868e231aa5eae20020903ebb68d2
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-09-15 16:14:06 +0200

    test: add test cases

--- TestModel/TestAddressBook.cs
+++ TestModel/TestAddressBook.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace CRM
 {
@@ -16,16 +17,73 @@ namespace CRM
         }
 
         [Test]
-        public void Contacts_AddOneContact_GetContact()
+        public void AddContacts_AddOneContact_GetContact()
         {
             //given
-            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16),"espagnole", "jp@gauthier.com");
+            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16), "espagnole", "jp@gauthier.com");
 
             //when
-            _addressBook.AddContact(gauthier);
+            _addressBook.AddContacts(new List<Contact> { gauthier });
 
             //then
             Assert.AreEqual(1, _addressBook.Contacts.Count);
+        }
+
+        [Test]
+        public void AddContacts_AddMultipleContacts_GetContacts()
+        {
+            //given
+            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16), "espagnole", "jp@gauthier.com");
+            Contact astier = new Contact("Astier", "Alexandre", new DateTime(1976, 05, 12), "française", "alex@astier.fr");
+
+            //when
+            _addressBook.AddContacts(new List<Contact> { gauthier, astier });
+
+            //then
+            Assert.AreEqual(2, _addressBook.Contacts.Count);
+        }
+
+        [Test]
+        public void Contacts_EmptyListOfContact_GetEmptyList()
+        {
+            //given
+            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16), "espagnole", "jp@gauthier.com");
+            Contact astier = new Contact("Astier", "Alexandre", new DateTime(1976, 05, 12), "française", "alex@astier.fr");
+
+            //when
+            //event is triggered during assertion
+
+            //then
+            Assert.AreEqual(0, _addressBook.Contacts.Count);
+        }
+
+        [Test]
+        public void Exists_ContactExists_GetContact()
+        {
+            //given
+            Contact astier = new Contact("Astier", "Alexandre", new DateTime(1976, 05, 12), "française", "alex@astier.fr");
+            _addressBook.AddContacts(new List<Contact> { astier });
+
+            //when
+            //event is triggered during assertion
+
+            //then
+            Assert.IsTrue(_addressBook.DoesExist(astier));
+        }
+
+        [Test]
+        public void AddContacts_DuplicateFound_ThrowException()
+        {
+            //https://docs.nunit.org/articles/nunit/writing-tests/assertions/classic-assertions/Assert.Throws.html
+            //given
+            Contact gauthier = new Contact("Gauthier", "Jean-Paul", new DateTime(2020, 12, 16), "espagnole", "jp@gauthier.com");
+            _addressBook.AddContacts(new List<Contact> { gauthier });
+
+            //when
+            Assert.Throws<ContactAlreadyExist>(() => _addressBook.AddContacts(new List<Contact> { gauthier }));
+
+            //then
+            //Exception thrown
         }
     }
 }
